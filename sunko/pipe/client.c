738ead3fa97affc3e35c778ba5451be967567ac3
seungwonme <seungwonan.me@gmail.com>
2023-06-15 21:32:31 +0900

    네임드파이프수정본

--- sunko/pipe/client.c
+++ sunko/pipe/client.c
@@ -1,29 +1,37 @@
+// client.c
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define FIFO_PATH "./SUNKO-FIFO"
 
 int main(void)
 {
-	int pd, n;
-	char inmsg[80];
+    int pd, n;
+    char inmsg[80];
 
-	if ((pd = open("./SUNKO-FIFO", O_RDONLY)) == -1)
-	{
-		perror("open");
-		exit(1);
-	}
-	printf("Client =====\n");
-	write(1, "From Server :", 13);
+    if ((pd = open(FIFO_PATH, O_RDONLY)) == -1)
+    {
+        perror("open");
+        exit(1);
+    }
 
-	while((n = read(pd, inmsg, 80)) > 0)
-		write(1, inmsg, n);
-	if (n == -1)
-	{
-		perror("write");
-		exit(1);
-	}
+    printf("Client =====\n");
+    write(1, "From Server :", 13);
 
-	write(1, "\n", 1);
-	close(pd);
+    while ((n = read(pd, inmsg, sizeof(inmsg))) > 0)
+        write(1, inmsg, n);
+
+    if (n == -1)
+    {
+        perror("read");
+        exit(1);
+    }
+
+    write(1, "\n", 1);
+    close(pd);
+
+    return 0;
 }
