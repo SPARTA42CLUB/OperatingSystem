2e1bf511ea4c2f76f0a24c9203ce6c9c49a06a07
Seungwon An <seunan@c6r3s8.42seoul.kr>
2023-06-16 19:18:22 +0900

    4장 스레드

---
+++ seunan/chapter_4/multi_threading/oneToOne.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREADS 3
+
+void* thread_function(void* arg) {
+    int thread_id = *((int*)arg);
+    printf("Hello from thread %d\n", thread_id);
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t threads[NUM_THREADS];
+    int thread_ids[NUM_THREADS] = {1, 2, 3};
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, thread_function, &thread_ids[i]);
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Main thread exiting\n");
+    return 0;
+}
